ad7a6b9d6c134703c20c34717eb5f1f62a643e49
Sidney Son <psndnb@gmail.com>
2019-05-31 01:31:43 -0700

    Backend almost done

--- testing/src/app/App.java
+++ testing/src/app/App.java
@@ -46,6 +46,9 @@ import org.apache.lucene.util.Version;
 import org.jsoup.Jsoup;
 import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 //import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,35 +56,52 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-//@RequestMapping("/api")
-//@CrossOrigin("*")
+@RequestMapping("/api")
+@CrossOrigin("*")
 public class App {
-	@GetMapping("/")
+	
+	@GetMapping("/articles")
     public List<Page> searchArticles (
-            @RequestParam(required=false, defaultValue="") String query) throws Exception{
-		System.out.println("TEST");
-//        if (query.isEmpty())
-//            return null;
-
-        Path f = Paths.get("C:\\Users\\psndn\\Documents\\GitHub\\webCrawler\\testing\\src\\app\\demo\\src\\main\\java\\CS172\\project\\demo\\indexFolder");
+            @RequestParam(required=false, defaultValue="usa") String query) throws Exception{
+		List<Page> matches = new ArrayList<>();
+		
+		// Check if no query
+        if (query.isEmpty())
+        	return matches;
+		
+        Path f = Paths.get("indexFolder");
     	StandardAnalyzer analyzer = new StandardAnalyzer();
     	IndexWriterConfig config = new IndexWriterConfig(analyzer);
     	Directory directory = FSDirectory.open(f);
     	
+    	//	CLEAN THE INDEX FOLDER BEFORE STARTING
+    	String indexLocation = System.getProperty("user.dir") + "/indexFolder";
+    	File index = new File(indexLocation);
+    	File[] files = index.listFiles();
+    	for (File file : files) {
+    		if (!file.delete()) { 
+    			System.out.println("Failed to delete "+file);
+    		}
+    	}
     	
-        //````````````````INDEX PART
+    	
+        // GATHER ALL THE FILES FROM DOWNLOAD FOLDER AND INDEX THEM
     	IndexWriter indexWriter = new IndexWriter(directory, config);
-    	
-        String data = new String(Files.readAllBytes(Paths.get("downloadFiles/File0.txt")));
-        String data2 = new String(Files.readAllBytes(Paths.get("downloadFiles/File1.txt")));
-    	org.jsoup.nodes.Document d = Jsoup.parse(data);
-    	org.jsoup.nodes.Document d2 = Jsoup.parse(data2);
-    	Page test = new Page(d.title(),d.body().text());
-    	Page test2 = new Page(d2.title(),d2.body().text());
     	List<Page> pages = new ArrayList<Page>();
-    	pages.add(test);
-    	pages.add(test2);
-    	
+    	String downloadLocation = System.getProperty("user.dir") + "/downloadFiles";
+    	File folder = new File(downloadLocation);
+    	String[] listOfFiles = folder.list();
+    	if(listOfFiles == null) {
+    		return matches;
+    	}
+		for (String name: listOfFiles) {
+	    	    // System.out.println(name);
+	    	    String data = new String(Files.readAllBytes(Paths.get("downloadFiles/" + name)));
+	    	    org.jsoup.nodes.Document d = Jsoup.parse(data);
+	    	    Page test = new Page(d.title(),d.body().text());
+	    	    pages.add(test);
+    	}
+    	    	
         for (Page page: pages) {
             Document doc = new Document();
             doc.add(new org.apache.lucene.document.TextField("title", page.title, Field.Store.YES));
@@ -89,7 +109,9 @@ public class App {
             indexWriter.addDocument(doc);
         }
         indexWriter.close();
-    	//```````````````IndexSearch PART
+    	
+        
+        // SCORE THE INDEXED FILES
         DirectoryReader indexReader = DirectoryReader.open(directory);
         IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 
@@ -98,18 +120,19 @@ public class App {
         boosts.put(fields[0], 1.0f);
         boosts.put(fields[1], 0.5f);
         MultiFieldQueryParser parser = new MultiFieldQueryParser(fields, analyzer, boosts);
-        Query q = parser.parse("usa");
+        Query q = parser.parse(query);
         int topHitCount = 100;
         ScoreDoc[] hits = indexSearcher.search(q, topHitCount).scoreDocs;
 
+        
         // Iterate through the results:
-        List<Page> matches = new ArrayList<>();
         for (int rank = 0; rank < hits.length; ++rank) {
             Document hitDoc = indexSearcher.doc(hits[rank].doc);
             Page p = new Page(hitDoc.get("title"), hitDoc.get("content"), hits[rank].score);
             matches.add(p);
         }
         indexReader.close();
+		
         directory.close();
         return matches;
     }
